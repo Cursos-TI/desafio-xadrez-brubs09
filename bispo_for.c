dafaf59f5639ccf38f083f571a454b7e98279fe3
brubs09 <brunamenina632009@gmail.com>
2025-06-02 18:57:01 +0000

    bispo_for

---
+++ bispo_for.c
@@ -0,0 +1,36 @@
+
+#include <stdio.h>
+
+#define TAM 8
+
+int main() {
+    char tabuleiro[TAM][TAM];
+
+    // Inicializa o tabuleiro com '.'
+    for (int i = 0; i < TAM; i++) {
+        for (int j = 0; j < TAM; j++) {
+            tabuleiro[i][j] = '.';
+        }
+    }
+
+    // Posição inicial do bispo
+    int linha = 5;  // Ex: linha 5
+    int coluna = 2; // Ex: coluna 2
+    tabuleiro[linha][coluna] = 'B';
+
+    // Movimento do bispo para cima e direita (↗)
+    for (int i = 1; linha - i >= 0 && coluna + i < TAM; i++) {
+        tabuleiro[linha - i][coluna + i] = '*'; // Marca o caminho
+    }
+
+    // Exibe o tabuleiro
+    printf("Movimento do bispo (↗):\n\n");
+    for (int i = 0; i < TAM; i++) {
+        for (int j = 0; j < TAM; j++) {
+            printf("%c ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
